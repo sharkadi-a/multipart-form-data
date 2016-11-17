d708450a04ce55b7e59267956aa2925b99c2ed73
Sharkadi Andrey <sharkadi.a@yandex.ru>
2016-11-17 23:37:59 +0300

    Первый коммит

---
+++ MultipartFormParser/MultipartForm.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace MultipartFormParser
+{
+    public class MultipartForm
+    {
+        private readonly Stream _stream;
+        private readonly Regex boundaryRegex = new Regex(@"(?<=boundary=)(.*?)(?=(\r\n\r\n)|\;)");
+        private Regex _nameRegex = new Regex(@"(?<=name\=\"")(.*?)(?=\"")");
+        private Regex _filenameRegex = new Regex(@"(?<=filename\=\"")(.*?)(?=\"")");
+        private Regex _contentTypeRegex = new Regex(@"(?<=Content\-Type:)(.*?)(?=(\r\n\r\n)|\;)");
+
+
+        private byte[] ReadBytes(Stream stream)
+        {
+            int b = 0;
+            IList<byte> bytes = new List<byte>(1000);
+            while ((b = stream.ReadByte()) != -1)
+            {
+                bytes.Add((byte) b);
+            }
+            return bytes.ToArray();
+        }
+
+        public MultipartForm(Stream stream)
+        {
+            if (stream == null) throw new ArgumentNullException("stream");
+            if (!stream.CanRead) throw new Exception("Stream should support reading");
+            _stream = stream;
+        }
+
+        public MultipartFormContent Parse()
+        {
+            if (!_stream.CanRead) throw new Exception("Stream should support reading");
+            using (var r = new StreamReader(_stream, Encoding.ASCII))
+            {
+                string line = null, boundary = null;
+                bool isMultipart = false;
+                while ((line = r.ReadLine()) != null)
+                {
+                    if (line.StartsWith("Content-Type:"))
+                    {
+                        isMultipart = true;
+                        if (!line.Contains("multipart/form-data")) throw new Exception();
+                        var boundaryMatch = boundaryRegex.Match(line);
+                        if (boundaryMatch.Success) boundary = boundaryMatch.Value;
+                    }
+                    if (string.IsNullOrWhiteSpace(line))
+                    {
+                        if (isMultipart) throw new Exception();
+                        if (!string.IsNullOrEmpty(boundary)) return ParseBoundary(boundary, r);
+                        continue;
+                    }
+                    if (isMultipart)
+                    {
+                        boundary = line;
+                        return ParseBoundary(boundary, r);
+                    }
+                }
+            }
+            throw new Exception();
+        }
+
+        private MultipartFormContent ParseBoundary(string boundary, StreamReader reader)
+        {
+            string line = null;
+            IList<MultipartFormContentItem> content = new List<MultipartFormContentItem>(10);
+            MultipartFormContentItem item = new MultipartFormContentItem();
+            var beginData = false;
+            StringBuilder data = new StringBuilder(1000);
+            while ((line = reader.ReadLine()) != null)
+            {
+                if (line.Contains(boundary))
+                {
+                    item.Content = Encoding.ASCII.GetBytes(data.ToString());
+                    content.Add(item);
+                    item = new MultipartFormContentItem();
+                    data = new StringBuilder(1000);
+                }
+                if (beginData)
+                {
+                    data.Append(line);
+                }
+                if (line.StartsWith("Content-Disposition:"))
+                {
+                    if (!line.Contains("form-data")) throw new Exception();
+                    var nameMatch = _nameRegex.Match(line);
+                    var filenameMatch = _filenameRegex.Match(line);
+                    if (nameMatch.Success) item.Name = nameMatch.Value;
+                    if (filenameMatch.Success) item.Filename = filenameMatch.Value;
+                }
+                if (line.StartsWith("Content-Type:"))
+                {
+                    var contentTypeMatch = _contentTypeRegex.Match(line);
+                    if (contentTypeMatch.Success) item.ContentType = contentTypeMatch.Value;
+                }
+                if (string.IsNullOrWhiteSpace(line)) beginData = true;
+            }
+            return new MultipartFormContent() {MultipartFormContentItems = content.ToArray()};
+        }
+    }
+}
---
+++ MultipartFormParser/UnitTests/UnitTests.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MultipartFormParser;
+using NUnit.Framework;
+
+namespace UnitTests
+{
+    [TestFixture]
+    public class UnitTests
+    {
+        private static string _multipart =
+@"POST / HTTP/1.1
+Host: localhost:8000
+User-Agent: Mozilla/5.0 (X11; Ubuntu; Linux i686; rv:29.0) Gecko/20100101 Firefox/29.0
+Accept: text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8
+Accept-Language: en-US,en;q=0.5
+Accept-Encoding: gzip, deflate
+Cookie: __atuvc=34%7C7; permanent=0; _gitlab_session=226ad8a0be43681acf38c2fab9497240; __profilin=p%3Dt; request_method=GET
+Connection: keep-alive
+Content-Type: _multipart/form-data; boundary=---------------------------9051914041544843365972754266
+Content-Length: 554
+
+-----------------------------9051914041544843365972754266
+Content-Disposition: form-data; name=""text""
+
+text default
+-----------------------------9051914041544843365972754266
+Content-Disposition: form-data; name=""file1""; filename=""a.txt""
+Content-Type: text/plain
+
+Content of a.txt.
+
+-----------------------------9051914041544843365972754266
+Content-Disposition: form-data; name=""file2""; filename=""a.html""
+Content-Type: text/html
+
+<!DOCTYPE html><title>Content of a.html.</title>
+
+-----------------------------9051914041544843365972754266--";
+
+        [Test]
+        public void Test()
+        {
+            var stream = new MemoryStream(Encoding.Default.GetBytes(_multipart));
+            var parser = new MultipartForm(stream);
+            var content = parser.Parse();
+        }
+    }
+}
