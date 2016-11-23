64e3a39e8bf816c436cbf504146d43b3937863ed
Sharkadi Andrey <sharkadi.a@yandex.ru>
2016-11-23 22:33:42 +0300

    Fix

---
+++ MultipartFormParser/MultipartFormDataItem.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Mime;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MultipartFormParser
+{
+    public class MultipartFormDataItem
+    {
+        public string Name { get; internal set; }
+        public string Filename { get; internal set; }
+        public string ContentType { get; internal set; }
+        public string Charset { get; internal set; }
+        public string ContentTransferEncoding { get; internal set; }
+        public byte[] Content { get; internal set; }
+
+        public MultipartFormDataItem()
+        {
+            Charset = "us-ascii";
+            ContentType = "text/plain";
+            ContentTransferEncoding = "7bit";
+        }
+    }
+}
